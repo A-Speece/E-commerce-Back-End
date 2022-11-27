05e02ed279a20c49390ea2f5f45bb141744af430
Anthony Speece <anthonyspeece@gmail.com>
2022-11-26 20:14:23 -0600

    added code for the tag route

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,23 +6,56 @@ const { Tag, Product, ProductTag } = require("../../models");
 router.get("/", (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: Product,
+  }).then((products) => {
+    res.json(products);
+  });
 });
 
 router.get("/:id", (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findByPk(req.params.id, {
+    include: Product,
+  }).then((product) => {
+    res.json(product);
+  });
 });
 
 router.post("/", (req, res) => {
   // create a new tag
+  Tag.create(req.body).then((product) => {
+    res.json(product);
+  });
 });
 
 router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(
+    req.body,
+    {
+      where: {
+        id: req.params.id,
+      },
+    },
+    {
+      new: true,
+    }
+  ).then((product) => {
+    res.json(product);
+  });
 });
 
 router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  }).then((product) => {
+    res.json(product);
+  });
 });
 
 module.exports = router;
